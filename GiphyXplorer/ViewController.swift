74af975600ec5cb68d3581cf25e3338ebc8514a5
Artem Kalinovsky <artem.kalinovsky@gmail.com>
2019-04-30 17:52:45 +0300

    Initial Commit

---
+++ GiphyXplorer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiphyXplorer
+//
+//  Created by Artem Kalinovsky on 4/30/19.
+//  Copyright © 2019 Artem Kalinovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
