c42f8f30a4dffe3e74829027bbe8dfa2812702ee
Artem Kalinovsky <artem.kalinovsky@gmail.com>
2019-05-14 12:14:51 +0300

    Dynamic gif cell background color

--- GiphyXplorer/View/GifObjectCollectionViewCell.swift
+++ GiphyXplorer/View/GifObjectCollectionViewCell.swift
@@ -16,12 +16,19 @@ final class GifObjectCollectionViewCell: UICollectionViewCell, BaseCellProtocol 
 
     private var currentGifDataRequest: DataRequest?
 
+    private static let backgroundColors = [UIColor(red: 255/255, green: 75/255, blue: 80/255, alpha: 1),
+                                           UIColor(red: 255/255, green: 246/255, blue: 61/255, alpha: 1),
+                                           UIColor(red: 11/255, green: 255/255, blue: 131/255, alpha: 1),
+                                           UIColor(red: 0/255, green: 191/255, blue: 255/255, alpha: 1),
+                                           UIColor(red: 134/255, green: 0/255, blue: 255/255, alpha: 1)]
+
     override func prepareForReuse() {
         super.prepareForReuse()
 
         currentGifDataRequest?.cancel()
         currentGifDataRequest = nil
         imageView.animatedImage = nil
+        imageView.backgroundColor = GifObjectCollectionViewCell.backgroundColors[Int.random(in: 0 ..< GifObjectCollectionViewCell.backgroundColors.count)]
     }
 
     func configure(with gifObject: GifObject) {
